e1c578dbd1f8ecfd4cbd3a1bb0321174e9ba54cb
Thomas <winningham@gmail.com>
2012-02-20 20:20:05 -0500

    moved test to test for test

--- src/rosettamesh/core.clj
+++ src/rosettamesh/core.clj
@@ -1,28 +1,3 @@
-(ns rosettamesh.core
-  (:gen-class)
-  (:use rosettamesh.toxi)
-  (:use rosettamesh.unlekker)
-  (:use rosettamesh.hemesh)
-  (:use rosettamesh.anar)
-  (:use rosettamesh.processing)
-)
+(ns rosettamesh.core)
 
-(import  
-  '(wblut.hemesh.creators HEC_Cone))
-
-(defn -main [& args]
-  (let [testMesh (fromHemesh (.create (HEC_Cone. 1 1 20 20)))]
-    (let [testAll
-    (fromProcessing
-      (toProcessing
-        (fromAnar
-          (toAnar
-            (fromModelBuilder
-              (toModelBuilder
-                (fromToxi
-                  (toToxi
-                    (fromHemesh
-                      (toHemesh testMesh)
-)))))))))
-])))
 
--- test/rosettamesh/test/core.clj
+++ test/rosettamesh/test/core.clj
@@ -1,6 +1,27 @@
 (ns rosettamesh.test.core
-  (:use [rosettamesh.core])
+  (:use rosettamesh.toxi)
+  (:use rosettamesh.unlekker)
+  (:use rosettamesh.hemesh)
+  (:use rosettamesh.anar)
+  (:use rosettamesh.processing)
   (:use [clojure.test]))
 
-(deftest replace-me ;; FIXME: write
-  (is false "No tests have been written."))
+(import  
+  '(wblut.hemesh.creators HEC_Cone))
+
+(deftest testAll
+  (let [testMesh (fromHemesh (.create (HEC_Cone. 1 1 20 20)))]
+    (let [testResult
+      (fromProcessing
+        (toProcessing
+          (fromAnar
+            (toAnar
+              (fromModelBuilder
+                (toModelBuilder
+                  (fromToxi
+                    (toToxi
+                      (fromHemesh
+                        (toHemesh testMesh))))))))))] 
+                          (is testResult "Something broke before here."))))
+
+
